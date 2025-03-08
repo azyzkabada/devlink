907df8b56e7d8c65049d247e1eb127699a6d4d12
azyzkabada <azyz.kabada@gmail.com>
2025-03-08 01:03:02 +0100

    urgence update2

--- app/page.tsx
+++ app/page.tsx
@@ -6,33 +6,35 @@ import AnimatedLogosHero from '@/components/layout/sections/hero';
 // import { HeroSection } from '@/components/layout/sections/hero';
 import GoogleMapStatic from '@/components/layout/sections/location';
 // import { ServicesSection } from '@/components/layout/sections/services';
-import { SponsorsSection } from '@/components/layout/sections/sponsors';
 import { TestimonialSection } from '@/components/layout/sections/testimonial';
 
 export const metadata = {
-  title: 'Shadcn - Landing template',
-  description: 'Free Shadcn landing page for developers',
+  title: 'Devlink - Empowering Brands & Clients',
+  description:
+    'Devlink provides top-tier branding and client management solutions.',
   openGraph: {
     type: 'website',
-    url: 'https://github.com/nobruf/shadcn-landing-page.git',
-    title: 'Shadcn - Landing template',
-    description: 'Free Shadcn landing page for developers',
+    url: 'https://devlinksolutions.ai',
+    title: 'Devlink - Empowering Brands & Clients',
+    description:
+      'Enhancing brand identity and optimizing customer relationships with tailored strategies.',
     images: [
       {
-        url: 'https://res.cloudinary.com/dbzv9xfjp/image/upload/v1723499276/og-images/shadcn-vue.jpg',
+        url: 'https://res.cloudinary.com/dbzv9xfjp/image/upload/v1723499276/og-images/devlink.jpg', // Remplacez par l'image officielle
         width: 1200,
         height: 630,
-        alt: 'Shadcn - Landing template',
+        alt: 'Devlink - Branding & Client Management Solutions',
       },
     ],
   },
   twitter: {
     card: 'summary_large_image',
-    site: 'https://github.com/nobruf/shadcn-landing-page.git',
-    title: 'Shadcn - Landing template',
-    description: 'Free Shadcn landing page for developers',
+    site: 'https://devlinksolutions.ai', // Remplacez par l'URL officielle
+    title: 'Devlink - Empowering Brands & Clients',
+    description:
+      'Enhancing brand identity and optimizing customer relationships with tailored strategies.',
     images: [
-      'https://res.cloudinary.com/dbzv9xfjp/image/upload/v1723499276/og-images/shadcn-vue.jpg',
+      'https://res.cloudinary.com/dbzv9xfjp/image/upload/v1723499276/og-images/devlink.jpg', // Remplacez par l'image officielle
     ],
   },
 };
